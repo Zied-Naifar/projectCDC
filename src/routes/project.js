3919766b7d4964336b5875cf8c917d215ca3d05c
Zied NAIFAR <zied.naifar@gmail.com>
2019-05-29 10:44:48 +0100

    valid doc upload

--- src/routes/project.js
+++ src/routes/project.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
+const upload = require("../config/upload");
 
 // Load Validation
 const validateProjectInput = require("../validation/project");
@@ -45,6 +46,7 @@ router.get("/:id", (req, res) => {
 // @access  Private
 router.post(
   "/create",
+  upload.single("document"),
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
     const { errors, isValid } = validateProjectInput(req.body);
@@ -63,7 +65,7 @@ router.post(
     if (req.body.organizer) projectFields.organizer = req.body.organizer;
     if (req.body.thematic) projectFields.thematic = req.body.thematic;
     if (req.body.description) projectFields.description = req.body.description;
-    if (req.body.document) projectFields.document = req.body.document;
+    if (req.file) projectFields.document = req.file.path;
 
     new Project(projectFields).save().then(project => res.json(project));
   }
@@ -74,6 +76,7 @@ router.post(
 // @access  Private
 router.put(
   "/update/:id",
+  upload.single("document"),
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
     const { errors, isValid } = validateProjectInput(req.body);
@@ -93,6 +96,7 @@ router.put(
     if (req.body.thematic) projectFields.thematic = req.body.thematic;
     if (req.body.description) projectFields.description = req.body.description;
     if (req.body.document) projectFields.document = req.body.document;
+    if (req.file) projectFields.document = req.file.path;
 
     Project.findOneAndUpdate(
       { _id: req.params.id },
